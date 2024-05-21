f586098a4c388e9dacc38f18cf9266d8f60aebf5
Pyroblazer <ignatiustimothymanullang@gmail.com>
2024-05-22 00:33:56 +0700

    install and configure passport, and implement post auth/login endpoint

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,13 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
-export class CreateUserDto {
-  @IsString()
-  @IsNotEmpty()
-  @ApiProperty()
-  name: string;
-
-  @IsString()
+export class LoginDto {
+  @IsEmail()
   @IsNotEmpty()
   @ApiProperty()
   email: string;
